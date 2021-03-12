b91ca9083fdcf0ff830383e30891c17ab1b3395b
Viresh99 <80322353+Viresh99@users.noreply.github.com>
2021-03-12 15:26:15 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,12 @@
+
+public class factorial {
+
+	public static void main(String[] args) {
+		int n=6,p=1;
+		for(int i=1;i<=n;i++)
+			p=p*i;
+		System.out.println("Factorial of 6 is: "+p);
+
+	}
+
+}
